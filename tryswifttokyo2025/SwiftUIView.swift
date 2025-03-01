98a130e911fff2585e0d98c919656be2f939f2ec
pradnya <pradnyanikam@gmail.com>
2025-03-01 17:00:04 +0800

    Update gradients as per time of the day and show title accordingly

--- tryswifttokyo2025/SwiftUIView.swift
+++ tryswifttokyo2025/SwiftUIView.swift
@@ -7,29 +7,71 @@
 
 import SwiftUI
 struct SwiftUIView: View {
-  @State private var gradientColors: [Color] = [Color.red, Color.blue]
+  @State private var currentGradientIndex = 0
+  @State private var title: String
+  private let titles = [
+  "Early Morning",
+  "Day",
+  "Evening",
+  "Night"]
+
+  init(currentGradientIndex: Int = 0) {
+    self.currentGradientIndex = currentGradientIndex
+    self.title = titles[currentGradientIndex]
+  }
+
+  private let gradients: [[Color]] = [
+//    [Color.orange, Color.yellow], // Early Morning
+//    [Color.blue, Color.white],     // Day
+//    [Color.purple, Color.red],     // Evening
+//    [Color.black, Color.blue]       // Night
+    [Color.red.opacity(0.8), Color.orange],
+    [Color.orange, Color.yellow],
+    [Color.pink.opacity(0.8), Color.orange],
+    [Color.purple, Color.blue]
+  ]
 
   var body: some View {
     ZStack {
-      LinearGradient(gradient: Gradient(colors: gradientColors), startPoint: .top, endPoint: .bottom)
+      LinearGradient(
+        gradient: Gradient(
+          stops: gradientStops(
+            for: gradients[currentGradientIndex])),
+        startPoint: .top,
+        endPoint: .bottom)
         .ignoresSafeArea()
         .onAppear {
           animateGradient()
         }
 
-      VStack {
-        Text("What time is it?")
+        Text(title)
           .font(.title)
-        Slider(value: .constant(7), in: 0...24)
-      }
+          .foregroundStyle(.white)
     }
   }
 
   private func animateGradient() {
-    // Change the colors after a delay
-    withAnimation(Animation.linear(duration: 3).repeatForever(autoreverses: true)) {
-      gradientColors = [Color.green, Color.purple]
+    Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+      withAnimation(.easeInOut(duration: 3), {
+        currentGradientIndex = (currentGradientIndex + 1) % gradients.count
+      }, completion: {
+        title = titles[currentGradientIndex]
+      })
     }
+  }
+
+  private func gradientStops(for colors: [Color]) -> [Gradient.Stop] {
+    return [
+      Gradient.Stop(color: colors[0], location: 0.0),
+      Gradient.Stop(color: colors[0], location: 0.6),
+      Gradient.Stop(color: colors[1], location: 1.0)
+    ]
+  }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+  static var previews: some View {
+    SwiftUIView()
   }
 }
 
