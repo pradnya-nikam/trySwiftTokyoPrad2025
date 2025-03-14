da281ec72d784ffe1898737f6cba19ea4cf3562e
pradnya <pradnyanikam@gmail.com>
2025-03-14 11:58:33 +0800

    Add new images for background and tree

--- tryswifttokyo2025/Ex2_CherryBlossom/SceneryView.swift
+++ tryswifttokyo2025/Ex2_CherryBlossom/SceneryView.swift
@@ -3,51 +3,23 @@ import SwiftUI
 struct SceneryView: View {
   var body: some View {
     ZStack {
-      Color(red: 0.53, green: 0.81, blue: 0.98)
+      Image("background")
+        .resizable().aspectRatio(contentMode: .fill)
         .edgesIgnoringSafeArea(.all)
+
       SkyView(numberOfClouds: 30)
-      VStack{
-        Spacer()
-          .frame(height: 350)
+
+      VStack {
+        Spacer().frame(height: 350)
         FallingPetalsView(numberOfPetals: 100)
       }
-      CherryBlossomTreeView()
+
+      Image("sakura_tree")
+        .resizable().aspectRatio(contentMode: .fill)
     }
   }
 }
 
 #Preview {
   SceneryView()
-}
-
-
-struct CherryBlossomTreeView: View {
-  private var imageWidth: CGFloat {
-    UIScreen.main.bounds.width/2 + 50
-  }
-  private var imageHeight: CGFloat {
-    UIScreen.main.bounds.height/2 + 40
-  }
-  var body: some View {
-    VStack {
-      Spacer()
-        .frame(height: 100)
-      HStack {
-        Spacer()
-          .frame(width: UIScreen.main.bounds.width - imageWidth)
-        Image("cherry-blossom-tree")
-          .resizable()
-          .aspectRatio(contentMode: .fill)
-          .frame(
-            minWidth: imageWidth,
-            maxWidth: .infinity,
-            minHeight: imageHeight,
-            maxHeight: .infinity,
-            alignment: .topLeading
-          )
-          .aspectRatio(2, contentMode: .fit)
-          .clipped()
-      }
-    }
-  }
 }
