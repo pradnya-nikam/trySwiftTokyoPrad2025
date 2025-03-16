b6e7ffb54639bac2019f3936fe3ec72fd8dc339c
pradnya <pradnyanikam@gmail.com>
2025-03-16 11:51:50 +0800

    Optimise petal view

--- tryswifttokyo2025/Ex2_CherryBlossom/FallingPetalsView.swift
+++ tryswifttokyo2025/Ex2_CherryBlossom/FallingPetalsView.swift
@@ -3,7 +3,6 @@ import SwiftUI
 struct PetalShape: Shape {
   func path(in rect: CGRect) -> Path {
     var path = Path()
-
     // Define the petal shape using a custom path
     path.move(to: CGPoint(x: rect.midX, y: rect.minY)) // Top point of the petal
     path.addCurve(to: CGPoint(x: rect.maxX, y: rect.midY), // Right curve
@@ -18,7 +17,6 @@ struct PetalShape: Shape {
     path.addCurve(to: CGPoint(x: rect.midX, y: rect.minY), // Back to the top point
                   control1: CGPoint(x: rect.minX, y: rect.midY), // Control point for top curve
                   control2: CGPoint(x: rect.minX, y: rect.minY)) // Control point for top curve
-
     return path
   }
 }
@@ -33,11 +31,12 @@ struct FallingPetal: View {
 
   var body: some View {
     PetalShape()
-      .fill(LinearGradient(gradient: Gradient(colors: [Color(red: 1.0, green: 0.75, blue: 0.8).opacity(0.8), Color(red: 1.0, green: 0.5, blue: 0.7).opacity(0.8)]), startPoint: .top, endPoint: .bottom))
+      .fill(LinearGradient(gradient: Gradient(colors: [Color(red: 1.0, green: 0.75, blue: 0.8), Color(red: 1.0, green: 0.5, blue: 0.7)]), startPoint: .top, endPoint: .bottom))
       .frame(width: 10, height: 5) // Petal size
       .opacity(0.8)
-      .position(position)
+//      .shadow(radius: 1)
 //      .drawingGroup()
+      .position(position)
       .onAppear {
         animatePetal()
       }
@@ -46,38 +45,45 @@ struct FallingPetal: View {
   public func animatePetal() {
     // Animate the petal falling down with a slight left drift
     withAnimation(
-      Animation.linear(
-        duration:
-          Double.random(in: 6...15)) // falling speed
-            .repeatForever(autoreverses: false)) {
-      position.y = UIScreen.main.bounds.height + 50
-      // horizontal drift for more leftward movement
-      offset = -CGFloat.random(in: 150...350)
-      position.x += offset
-    }
+      Animation.linear(duration: Double.random(in: 6...15)) // falling speed
+        .repeatForever(autoreverses: false)) {
+          position.y = UIScreen.main.bounds.height + 50
+          // horizontal drift for more leftward movement
+          offset = -CGFloat.random(in: 150...350)
+          position.x += offset
+        }
   }
 }
 
 struct FallingPetalsView: View {
   let numberOfPetals: Int
-  @State private var petalCount: Int = 0
+  @State private var petalPositions: [CGPoint] = []
 
   var body: some View {
     ZStack {
-      ForEach(0..<petalCount, id: \.self) { index in
-        // Petal appears on the right side of the screen
-        let startX = CGFloat.random(in: UIScreen.main.bounds.width * 0.5...UIScreen.main.bounds.width)
-        let startY = CGFloat.random(in: 0...50)
-        FallingPetal(startPosition: CGPoint(x: startX, y: startY))
+      ForEach(petalPositions.indices, id: \.self) { index in
+        FallingPetal(startPosition: petalPositions[index])
       }
     }
     .onAppear {
+      // Start generating more petals
       Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
-        if petalCount < numberOfPetals {
-          petalCount += 1
+        if petalPositions.count < numberOfPetals {
+          let newPosition = randomPosition()
+          petalPositions.append(newPosition)
+        }
+        // Remove petals that have fallen off the screen
+        petalPositions.removeAll { position in
+          position.y > UIScreen.main.bounds.height
         }
       }
     }
+  }
+
+  private func randomPosition() -> CGPoint {
+    let startX = CGFloat.random(in: UIScreen.main.bounds.width * 0.5...UIScreen.main.bounds.width)
+    let startY = CGFloat.random(in: 0...50)
+    return CGPoint(x: startX, y: startY)
   }
 }
 
